23bd105ac0287bdf9fcf0bb2258721c929f64921
Balint Erdi <balint@balinterdi.com>
2022-11-18 18:52:17 +0100

    Exercise 4: Specialty story grids

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { COLORS, QUERIES } from '../../constants';
 
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
@@ -47,14 +48,35 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+  @media(${QUERIES.desktopAndUp}) {
+    grid-template-columns: 1fr 1fr;
+    gap: 0px;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media(${QUERIES.desktopAndUp}) {
+    padding-right: 1rem;
+    border-right: 1px solid ${COLORS.gray['300']};
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+`;
 
-const SportsSection = styled.section``;
+const SportsSection = styled.section`
+  @media(${QUERIES.desktopAndUp}) {
+    padding-left: 1rem;
+  }
+`;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+`;
 
 export default SpecialtyStoryGrid;
